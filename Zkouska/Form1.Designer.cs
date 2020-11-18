fe26fc53f3d611ebb96ff9bb3c5344f990160e23
tomas sandera <tomipaci1230@gmail.com>
2020-11-18 09:21:39 +0100

    Hotovo

--- Zkouska/Form1.Designer.cs
+++ Zkouska/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(170, 35);
+            this.button1.Location = new System.Drawing.Point(170, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -48,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(170, 61);
+            this.button2.Location = new System.Drawing.Point(170, 64);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
